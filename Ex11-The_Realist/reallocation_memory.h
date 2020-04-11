ade281920d541aea8332ec28264ee1a219d4772c
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-11 16:40:43 +0300

    add header file for reallocation_memory.c

---
+++ Ex11-The_Realist/reallocation_memory.h
@@ -0,0 +1,11 @@
+#define reallocarion_memory_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define INITIAL_SIZE_OF_ARRAY (10)
+#define END_OF_ELEMENTS (-1)
+#define INITIAL_INDEX (0)
+#define ERROR_MEMORY_ALLOCATION_FAILED ("Error! memory allocation failed.\n")
